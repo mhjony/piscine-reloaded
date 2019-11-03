dc48db35fa3a2725969365ab7b2a38cc6ddeeaac
mahmudulhasanjhoony <mahmudul.jhony@gmail.com>
2019-11-03 13:14:17 +0200

    Makefile added

---
+++ ex27/ft_display_file.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display_file.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhasan <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/10/17 16:08:40 by mhasan            #+#    #+#             */
+/*   Updated: 2019/10/18 11:41:17 by mhasan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <fcntl.h>
+
+int	main(int argc, char **argv)
+{
+	int		fd;
+	char	c;
+
+	if (argc == 1)
+		write(2, "File name missing.\n", 19);
+	else if (argc > 2)
+		write(2, "Too many arguments.\n", 20);
+	else
+	{
+		fd = open(argv[1], O_RDONLY);
+		if (fd == -1)
+			return (1);
+		while (read(fd, &c, 1))
+			write(1, &c, 1);
+		if (close(fd) == -1)
+			return (1);
+	}
+	return (0);
+}
